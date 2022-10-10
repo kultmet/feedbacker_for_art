2f5a3d018ad29af0959ab18c046f5c8c93516990
Ольга Панайоти <olga.panayoti@gmail.com>
2022-10-10 16:19:19 +0300

    ? request.user

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -18,17 +18,18 @@ from rest_framework import permissions
 
 class IsAdminOrReadOnly(permissions.BasePermission):
 
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            # or request.user.is_authenticated
-            # or request.user.role == 'admin'
-            # or request.user.is_staff
-            # or request.user.is_superuser
-        )
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return request.user == 'admin'
+    # def has_permission(self, request, view):
+    #     return (
+    #         request.method in permissions.SAFE_METHODS
+    #         # or request.user.is_authenticated
+    #         # or request.user.role == 'admin'
+    #         # or request.user.is_staff
+    #         # or request.user.is_superuser
+    #     )
+    #
+    # def has_object_permission(self, request, view, obj):
+    #     if request.method in permissions.SAFE_METHODS:
+    #         return True
+    #
+    #     return request.user == 'admin'
+    pass
--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -90,10 +90,22 @@ class ReviewViewSet(viewsets.ModelViewSet):
     # permission_classes = (AuthorOrModeratorOrAdminOrReadOnly,)
 
     def get_queryset(self):
+        print('$$$$$$$$$$$$$$$$')
+        print(self.kwargs['title_id'])
+        print(self.kwargs.get('title_id'))
         title = get_object_or_404(Title, id=self.kwargs['title_id'])
+        print('!!!!!!!!!!!!!!!')
+        print(title)
+        user = self.request.user
+        print(self.request)
+        print(self.get_queryset(id='user'))
+        print(user)
+        print(user.id)
         return title.reviews.all()
 
     def perform_create(self, serializer):
+        print('&&&&&&&&&&&&&&&&')
+        print(self.request.user_id)
         title = get_object_or_404(Title, id=self.kwargs['title_id'])
         serializer.save(author=self.request.user, title=title)
 
@@ -114,6 +126,10 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
         review_id = self.kwargs.get('review_id')
+        print('@@@@@@@@@@@@@@@@@@@@@@@@')
+        print(title_id)
+        print(review_id)
+        print(self.request.user)
         review = get_object_or_404(Review, id=review_id, title=title_id)
         serializer.save(author_id=self.request.user, review=review)
 
