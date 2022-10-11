bca74acd210058aa5a956ee4a6238b10be2ffd47
Ольга Панайоти <olga.panayoti@gmail.com>
2022-10-11 16:49:34 +0300

    47 passed  review model  confirmation_cAde  pagination - 4

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -167,5 +167,5 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 4,
 }
